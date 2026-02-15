c272c4d4e160641f18d011d3bc26f7f3e9e6ec44
CheatSheetStats <shelbdixon111@gmail.com>
2026-02-15 07:26:05 +0000

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -401,6 +401,7 @@ else:
             'PredictionBTTS', 'Over25YN', 
             'Home Clean Sheet %', 'Away Clean Sheet %',
             'PPG Δ', 'Form Δ',
+            'Home form PPG', 'Away form PPG'
             'Home xG', 'Away xG',
             'BTTS %', 'Over 2.5 Goals %'
         ]
